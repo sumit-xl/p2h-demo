8d33f3d6f59b0667b5a7bcb157d8fc80fb1df0e2
Sumit Sharma <sumit@xhtmllive.com>
2019-02-02 19:21:40 +0530

    Page completed

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,7 +6,7 @@ jQuery( document ).ready( function( $ ) {
 
 	$( '.header__menu-toggle' ).on( 'click', function( event ) {
 		event.preventDefault();
-		$( 'body' ).toggleClass( 'is-active' );
+		$( '.header__menu-toggle' ).toggleClass( 'is-active' );
 		$( '.header nav' ).slideToggle( 'fast' );
 	} );
 
@@ -18,16 +18,21 @@ jQuery( document ).ready( function( $ ) {
 		}
 	} );
 
-
 	$( '.js-toggle-language' ).on( 'click', function( event ) {
 		event.preventDefault();
 		$( '.footer__language' ).toggleClass( 'is-active' );
-		$( '.footer__language ul' ).slideToggle('fast');
+		$( '.footer__language ul' ).slideToggle( 'fast' );
 	} );
 
 	$( '.footer__language ul li a' ).on( 'click', function() {
 		event.preventDefault();
-		$( '.footer__language ul' ).slideUp('fast');
+		$( '.footer__language ul' ).slideUp( 'fast' );
 	} );
+
+	$('.js-slider').slick({
+		infinite: false,
+		dots: true,
+		rows: 0
+	});
 
 } );
